c66b87c1fbb5d0512333095a7296f426bcaa1ad4
11399997911 <11399997911@qq.com>
2018-03-08 14:22:46 +0800

    测试demo

--- src/AASDA.java
+++ src/AASDA.java
@@ -1,2 +1,16 @@
 public class AASDA {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("我想飞山田看= 按实际的骄傲的借款方会计分录");
+
+
+
+        int  i  =0;
+        int a =5;
+        System.out.println(i-a);
+
+    }
+
 }
