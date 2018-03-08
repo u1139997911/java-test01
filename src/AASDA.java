cd0e734f39c9843597b8f9a2bb45c80f66154519
11399997911 <11399997911@qq.com>
2018-03-08 15:09:24 +0800

    山超级爱你待多久啊

--- src/AASDA.java
+++ src/AASDA.java
@@ -3,12 +3,12 @@ public class AASDA {
     public static void main(String[] args) {
 
 
-        System.out.println("我想飞山田看= 按实际的骄傲的借款方会计分录");
-        System.out.println("dasdasd");
 
         int  i  =0;
         int a =5;
         System.out.println(i-a);
+
+
 
     }
 
